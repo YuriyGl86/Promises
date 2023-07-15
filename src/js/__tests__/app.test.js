0adb7c9947035780f0b82876fd957682b3e37502
Yuriy Glushchenko <yuriygl86@gmail.com>
2023-07-15 10:47:20 +0300

    fix

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,7 +1,13 @@
-import GameSavingLoader from "../app";
+import GameSavingLoader from '../app';
 
-test('wfwef', async (done) => {
-    const data = await GameSavingLoader.load()
-    expect(data).toEqual({"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}})
-    done()
-})
+test('testing promise', async (done) => {
+  const data = await GameSavingLoader.load();
+  expect(data).toEqual({
+    id: 9,
+    created: 1546300800,
+    userInfo: {
+      id: 1, name: 'Hitman', level: 10, points: 2000,
+    },
+  });
+  done();
+});
--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,14 @@
 // TODO: write your code here
 
-import read from "./reader.js"
-import json from "./parser.js"
+import read from './reader';
+import json from './parser';
 
 export default class GameSavingLoader {
   static load() {
     // const data = read(); // возвращается Promise!
     // const value = json(data); // возвращается Promise!
-    return read().
-    then(data => json(data)).
-    then(jsonString => JSON.parse(jsonString))
+    return read()
+      .then((data) => json(data))
+      .then((jsonString) => JSON.parse(jsonString));
   }
 }
-
-GameSavingLoader.load().then(console.log)
